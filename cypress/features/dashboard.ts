a1e8569c4c1cea29bf5f15e703f6440c3b53337e
Adrien Mazet <mazet.a@sfeir.com>
2022-12-14 16:00:22 +0100

    Test files creation

---
+++ cypress/features/dashboard.ts
@@ -0,0 +1,5 @@
+// check that list can display many items
+// check that each card display title + stock symbol change, opening price, current price, high price
+// check that each card as an arrow corresponding to change
+// check that each card as a delete button that remove the card when clicked
+// check that each card as a go to ... button that redirects to correct URL
---
+++ cypress/features/sentiments.ts
@@ -0,0 +1,5 @@
+// check that there is a back to list of stock button that redirects to correct url
+// check that there is the stock title and symbol corresponding to url
+// check that it display month name + change + mspr + arrow for each month
+// check that it display no data for other months for months with no data
+// maybe intercept or mock ?
